3820fe60c885a2cfb272f1fb67a215ed170a5d45
Ramya <tadala1999@gmail.com>
2022-04-19 19:47:38 +0530

    simple increment app created by changing some code

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 // document.getElementById("count-el").innerText = 5
 
-/*let count = 0
-
-console.log(count)
-
-let myAge
-
-myAge = 23
-
-console.log(myAge)*/
-
-/*let myAge = 23
-let humanDogRatio = 7
-myDogAge = myAge * humanDogRatio
-console.log(myDogAge)*/
 let countEl = document.getElementById("count-el")
-//console.log(countEl)
+let saveEl = document.getElementById("save-el")
 
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
